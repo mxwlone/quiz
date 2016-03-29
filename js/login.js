31fa176bceeeacfcc59fc13d6629612f1cddd0a5
mxwlone <1ax@gmx.net>
2016-03-29 18:16:53 +0200

    Fixed login

--- js/login.js
+++ js/login.js
@@ -6,35 +6,61 @@
 $(document).ready(function() {
     $('#login').click(auth);
     $('#signup').click(register);
+
+    $.getJSON("json/users.json", function(json) {
+        if (!localStorage.users) {
+            localStorage.users = JSON.stringify(json.users);
+            console.log('Local storage for users created.')
+        }
+    });
+
+    var params = window.location.search.substr(1);
+    if (params) {
+        if (params.split('=')[0] === 'msg') {
+            $('.status-text').html(decodeURIComponent(params.split('=')[1]));
+        }
+    }
 });
 
 function register() {
-    location.replace("register.html");
+    location.replace("signup.html");
 }
 
 function auth() {
     var email = $('#email').val();
     var password = $('#password').val();
+    var cookie = $('#remember-box').is(':checked');
 
-    var users;
     $.getJSON("json/users.json", function(json) {
-        console.log(json);
-        for (var user in json.users) {
-            if(email === json.users[user]["email"] && password == json.users[user]["password"]) {
-                console.log(email + " authenticated.")
-                return login(json.users[user])
+        //console.log(json);
+        var users;
+        if (!localStorage.users) {
+            localStorage.users = JSON.stringify(json.users);
+            console.log("creating local storage (users)");
+            users = json.users;
+        } else {
+            users = JSON.parse(localStorage.users);
+        }
+
+        //console.log(JSON.stringify(users));
+        for (var user in users) {
+            if(email === users[user]["email"] && password == users[user]["password"]) {
+                console.log(email + " authenticated.");
+                return login(users[user]);
             }
         }
 
-        $('.status-login').html("Login failed.");
+        $('.status-text').html("Login failed.");
     });
+
 }
 
 function login(user) {
-    console.log("Login with object");
+    console.log("Login with object:");
     console.log(user);
     sessionStorage.user = user.firstname + " " + user.lastname;
     sessionStorage.email = user.email;
 
-    location.replace("index.html")
+    location.replace("index.html");
+    return false;
 }
